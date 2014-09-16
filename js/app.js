23c0aa0583895f2e2304450b0d040ef092cf449c
justin hardin <jpmitchell293@gmail.com>
2014-09-15 20:14:28 -0700

    populates page with instagram photos using the kicks4sale hashtag

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 $(document).foundation();
+
+    var feed = new Instafeed({
+        get: 'tagged',
+        tagName: 'Kicks4sale',
+        clientId: '45c8e1b222ca4cfc9121a6d9f5d1f5ae'
+    });
+    feed.run();
