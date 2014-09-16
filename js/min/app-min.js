5077b7f1500e05b1f825f7e918d46f709c1f032f
justin hardin <jpmitchell293@gmail.com>
2014-09-15 22:52:01 -0700

    fixed padding and added one column mobile option

--- js/min/app-min.js
+++ js/min/app-min.js
@@ -1,1 +1,1 @@
-$(document).foundation();var feed=new Instafeed({get:"tagged",tagName:"Kicks4sale",clientId:"45c8e1b222ca4cfc9121a6d9f5d1f5ae"});feed.run();
+$(document).foundation();var feed=new Instafeed({get:"tagged",tagName:"Kicks4sale",clientId:"45c8e1b222ca4cfc9121a6d9f5d1f5ae",target:"photo-grid",limit:60,resolution:"standard_resolution",template:'<a class = "large-4 medium-4 columns photo" href="{{link}}"><img src="{{image}}" /></a>'});feed.run();
