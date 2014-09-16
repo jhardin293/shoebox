17d7bb767d561be7397034ac0912a29d7aadf263
justin hardin <jpmitchell293@gmail.com>
2014-09-15 23:21:15 -0700

    added show more button. But when clicked it scrolls to top.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 /*global Instafeed:true*/
 $(document).foundation();
 
-    var feed = new Instafeed({
-        get: 'tagged',
-        tagName: 'Kicks4sale',
-        clientId: '45c8e1b222ca4cfc9121a6d9f5d1f5ae',
-        target: 'photo-grid',
-        limit: 60,
-        resolution: 'standard_resolution',
-        template: '<a class = "large-4 medium-4 columns photo" href="{{link}}"><img src="{{image}}" /></a>'
-    });
-    feed.run();
+var loadButton = document.getElementById('load-more');
+
+var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'Kicks4sale',
+    clientId: '45c8e1b222ca4cfc9121a6d9f5d1f5ae',
+    target: 'photo-grid',
+    limit: 60,
+    resolution: 'standard_resolution',
+    template: '<a class = "large-4 medium-4 columns photo" href="{{link}}"><img src="{{image}}" /></a>',
+    after: function () {
+    	if (!this.hasNext()) {
+    		loadButton.setAttribute('disabled', 'disabled');
+    	}
+    },
+});
+
+loadButton.addEventListener('click', function() {
+  feed.next();
+});
+feed.run();
